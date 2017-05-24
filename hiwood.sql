305ed1504a2cc6f55bd8231f912f0396c62da2d6
borovlev <borovlev93@gmail.com>
2017-05-25 02:47:36 +0300

    Add files via upload

---
+++ hiwood.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 25 2017 г., 01:46
+-- Версия сервера: 10.1.21-MariaDB
+-- Версия PHP: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `hiwood`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `details`
+--
+
+CREATE TABLE `details` (
+  `id` int(11) NOT NULL,
+  `id_faculty` int(11) NOT NULL,
+  `id_students` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `name`) VALUES
+(1, 'Robotics'),
+(2, 'Nano Surgery'),
+(3, 'Engineering');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `id` int(11) NOT NULL,
+  `Day` date NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `id_teachers` int(11) NOT NULL,
+  `id_faculty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `schedule`
+--
+
+INSERT INTO `schedule` (`id`, `Day`, `subject`, `id_teachers`, `id_faculty`) VALUES
+(17, '2017-05-02', 'mathematics', 1, 1),
+(18, '2017-05-02', 'mathematics', 2, 2),
+(19, '2017-05-02', 'mathematics', 3, 3),
+(23, '2017-05-03', '', 2, 1),
+(24, '2017-05-03', '', 3, 2),
+(25, '2017-05-03', '', 5, 3),
+(26, '2017-05-04', '', 3, 1),
+(27, '2017-05-04', '', 5, 2),
+(28, '2017-05-04', '', 1, 3),
+(29, '2017-05-05', '', 5, 1),
+(30, '2017-05-05', '', 1, 2),
+(31, '2017-05-05', '', 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `faculty_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `surname`, `gender`, `faculty_id`) VALUES
+(65, 'Artem', 'Bor', 'male', 3),
+(66, 'Artem', 'Asdasda', 'male', 1),
+(67, 'Artem', 'Asdasda', 'male', 1),
+(68, 'Artem', 'Asdasda', 'male', 1),
+(69, 'Artem', 'Asdasda', 'male', 1),
+(70, 'Artem', 'Asdasda', 'male', 1),
+(71, 'Artem', 'Asdasda', 'male', 1),
+(72, 'Artem', 'Asdasda', 'male', 1),
+(73, 'Artem', 'Asdasda', 'male', 1),
+(74, 'Sfd', 'Dfs', 'female', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `teachers`
+--
+
+INSERT INTO `teachers` (`id`, `name`, `surname`) VALUES
+(1, 'Jon', 'Black'),
+(2, 'Jon', 'White'),
+(3, 'Jon ', 'Grey'),
+(5, 'Jon', 'Red');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `details`
+--
+ALTER TABLE `details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_faculty` (`id_faculty`),
+  ADD KEY `id_students` (`id_students`);
+
+--
+-- Индексы таблицы `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_teachers` (`id_teachers`),
+  ADD KEY `id_faculty` (`id_faculty`);
+
+--
+-- Индексы таблицы `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `faculty_id` (`faculty_id`);
+
+--
+-- Индексы таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `details`
+--
+ALTER TABLE `details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT для таблицы `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+--
+-- AUTO_INCREMENT для таблицы `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `details`
+--
+ALTER TABLE `details`
+  ADD CONSTRAINT `details_ibfk_1` FOREIGN KEY (`id_faculty`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `schedule`
+--
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`id_teachers`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `schedule_ibfk_2` FOREIGN KEY (`id_faculty`) REFERENCES `faculty` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
